9175a05aa398045b7d2e8d95649de9984cf0e8f6
JamieCranston <jcranston@protonmail.com>
2021-05-28 17:27:07 +0100

    clean out redundant code and add function cutting species_long to species_shown

--- r/clean_survey_data.R
+++ r/clean_survey_data.R
@@ -120,3 +120,17 @@ strings_as_factors <- function(data) {
   return(data)
   
 }
+
+
+
+check_species_shown_by_id <- function(speciesdata, respondentdata) {
+  
+  species_shown <- respondentdata %>%
+    dplyr::select(id, contains("verte")) %>%
+    tidyr::pivot_longer(cols = contains("verte"), values_to = "species") %>% 
+    dplyr::select(-name) %>% 
+    dplyr::left_join(., speciesdata)
+  
+  
+  return(species_shown)
+}
