143e203e13c1a5c442cf48e62b87856f41935fd2
JamieCranston <jcranston@protonmail.com>
2021-05-29 09:00:22 +0100

    add getOrdinalNumber function to utils.R

--- r/utils.R
+++ r/utils.R
@@ -5,3 +5,17 @@ load_config <- function(){
   
   return(config)
 }
+
+getOrdinalNumber <- function(num) {
+  result <- ""
+  if (!(num %% 100 %in% c(11, 12, 13))) {
+    result <- switch(as.character(num %% 10), 
+                     "1" = {paste0(num, "st")}, 
+                     "2" = {paste0(num, "nd")},
+                     "3" = {paste0(num, "rd")},
+                     paste0(num,"th"))
+  } else {
+    result <- paste0(num, "th")
+  }
+  return(result)
+}
