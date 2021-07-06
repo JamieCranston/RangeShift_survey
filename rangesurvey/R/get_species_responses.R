b77d6cc6a3c98d53625ff7ea8e8d7e959648ec19
JamieCranston <36704853+JamieCranston@users.noreply.github.com>
2021-07-06 11:10:59 +0100

    Squash this into last commit

---
+++ rangesurvey/R/get_species_responses.R
@@ -0,0 +1,29 @@
+#' get_species_responses
+#'
+#' @param data whole survey data
+#'
+#' @return species responses data
+#' @export
+#'
+get_species_responses <- function(data) {
+  data <- data %>%
+    dplyr::select(
+      -c(
+        dplyr::contains("GRecorderInfo"),
+        dplyr::contains("Gawareness"),
+        dplyr::contains("ertEq"),
+        "ClimateEq",
+        dplyr::contains("VoN"),
+        dplyr::contains("associations"),
+        dplyr::contains("GDemographics"),
+        dplyr::contains("Array"),
+        dplyr::contains("Dual"),
+        "FinalClimateCheck",
+        "GFreeFromFinal"
+      )
+    ) %>%
+    dplyr::mutate(id = as.factor(.data$id))
+  
+  
+  return(data)
+}
