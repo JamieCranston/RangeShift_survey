3b6d057c4b21d7c5eca705d06b2a203206d502f9
JamieCranston <36704853+JamieCranston@users.noreply.github.com>
2021-07-06 10:58:49 +0100

    rename R script to match function name and remove underscore typo

---
+++ rangesurvey/R/is_species_in_recorded_group.R
@@ -0,0 +1,87 @@
+#' @title is_species_in_recorded_group
+#'
+#' @param speciesdata cleaned species responses data
+#' @param respondentdata cleaned respondent data
+#'
+#' @return
+#' @export
+#'
+is_species_in_recorded_group <- function(speciesdata,
+                                          respondentdata = respondent_character_table_clean) {
+  vertnames <- c(
+    "Egarzetta",
+    "Pleucorodia",
+    "Aalba",
+    "Bibis",
+    "Hhimantopus",
+    "Pfalcinellus",
+    "Iminutus",
+    "Apurpurea"
+  )
+
+  odonames <- c(
+    "Eviridulum",
+    "Aaffinis"
+  )
+
+  lepinames <- c(
+    "Calgae",
+    "Eornata"
+  )
+  hymennames <- c(
+    "Dsaxonica",
+    "Bhypnorum"
+  )
+
+  hemipnames <- c(
+    "Rnebulosa",
+    "Pkadenii"
+  )
+
+  match_dat <- respondentdata %>%
+    dplyr::select(
+      .data$id,
+      .data$Hymenoptera,
+      .data$Lepidoptera,
+      .data$Hemiptera,
+      .data$Odonata,
+      .data$Birds,
+      dplyr::contains("vert")
+    ) %>%
+    tidyr::pivot_longer(dplyr::contains("vert"),
+                        values_to = "species") %>%
+    dplyr::mutate(match = as.factor(dplyr::case_when(
+      species %in% vertnames & Birds == "Yes" |
+        species %in% hemipnames & Hemiptera == "Yes" |
+        species %in% hymennames & Hymenoptera == "Yes" |
+        species %in% lepinames & Lepidoptera == "Yes" |
+        species %in% odonames & Odonata == "Yes"
+      ~ "Yes",
+      species %in% vertnames & Birds == "No" |
+        species %in% hemipnames & Hemiptera == "No" |
+        species %in% hymennames & Hymenoptera == "No" |
+        species %in% lepinames & Lepidoptera == "No" |
+        species %in% odonames & Odonata == "No"
+      ~ "No",
+      TRUE ~ NA_character_
+    ))) %>%
+    dplyr::mutate(taxa = as.factor(dplyr::case_when(
+      species %in% vertnames ~ "vertebrate",
+      !species %in% vertnames ~ "invertebrate"
+    ))) %>%
+    dplyr::select(
+      -.data$name,
+      -.data$Hymenoptera,
+      -.data$Lepidoptera,
+      -.data$Hemiptera,
+      -.data$Odonata,
+      -.data$Birds
+    )
+
+  speciesdata <- dplyr::left_join(
+    speciesdata,
+    match_dat
+  )
+
+  return(speciesdata)
+}
