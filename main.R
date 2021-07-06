d93ce98400bc861bf4fd2140c12c2188ebc3dde7
JamieCranston <36704853+JamieCranston@users.noreply.github.com>
2021-07-06 10:55:43 +0100

    Fix some typos and condense function for data cleaning

--- main.R
+++ main.R
@@ -2,25 +2,23 @@
 library(rangesurvey)
 library(magrittr)
 
-config <- load_config()
+ggplot2::theme_set(load_theme())
 
+config <- load_config()
 
 # data cleaning -----------------------------------------------------------
 
 # Load in raw survey data
 survey_data_raw <- ingest_survey_data(config)
 
-# Remove pilot survey responses and incomplete surveys
-survey_data_complete_responses <- survey_data_raw %>%
-  remove_pilot_responses() %>%
-  remove_incomplete_surveys()
+# Clean survey data, remove:
+# - pilot survey responses
+# - incomplete surveys
+# - columns showing metainformation
+# - question response times
+# - survey instructions
 
-# Remove columns showing metainformation, question response times and survey
-# instructions
-survey_data_clean <- survey_data_complete_responses %>%
-  remove_meta_info() %>%
-  remove_response_times() %>%
-  remove_instructions()
+survey_data_clean <- clean_survey_data(data = survey_data_raw)
 
 # Split raw survey data to begin building two tidy datasets: one for respondent
 # info, one for species responses.
@@ -51,22 +49,27 @@ respondent_table_clean <-
   check_awareness(., config) %>%
   check_recorder_role(., config) %>%
   check_groups_recorded(., config) %>%
-  scale_numerics()
+  scale_numerics() %>% 
+  dplyr::mutate(awareness_numeric = score_awareness(numeric_out = TRUE) %>% 
+                  dplyr::pull(awareness_score))
 
 #reorder_table_columns()
-
 
 # begin clean species responses
 survey_responses_to_model  <- survey_species_responses %>%
   pivot_species_long() %>%
   check_species_shown_by_id(speciesdata = .,
                             respondentdata = respondent_table_clean) %>%
-  is_species_in__recorded_group(speciesdata = .,
+  is_species_in_recorded_group(speciesdata = .,
                                 respondentdata = respondent_table_clean) 
 
-species_responses_to_model <- survey_responses_to_model%>%
+species_responses_to_model <- survey_responses_to_model %>%
   likert_7_to_3() %>%
-  dplyr::select(id, species, attitude_to_species, seen, match)
+  dplyr::select(id,
+                species,
+                attitude_to_species,
+                seen,
+                match)
 
 management_responses_to_model <-  survey_responses_to_model %>%
   likert_7_to_3() %>%
@@ -83,6 +86,10 @@ management_responses_to_model <-  survey_responses_to_model %>%
 
 # visualise model input data ----------------------------------------------
 
+# number of responses by species
+survey_responses_to_model %>%
+  dplyr::count(species)
+
 plot_fig_1(survey_responses_to_model)
 plot_fig_3(survey_responses_to_model)
 
@@ -94,7 +101,7 @@ plot_supp_fig_3(data = respondent_table_clean)
 plot_supp_fig_4(data = respondent_table_clean)
 plot_supp_fig_5(data = respondent_table_clean,
                 config =  config)
-plot_supp_fig_6(survey_responses_to_model)
+plot_supp_fig_6(speciesdata = survey_responses_to_model)
 plot_supp_fig_7(data = respondent_table_clean)
 
 # build models for respondents' attitudes to species and management -------
@@ -122,3 +129,5 @@ plot_supp_table_2_figures_insets()
 plot_supp_table_3_figures_insets()
 
 save_output_figures()
+
+# render supplementary figures doc------------------
