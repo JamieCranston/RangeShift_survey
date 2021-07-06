365680da61b8b410575e7f996063d075f547fada
JamieCranston <36704853+JamieCranston@users.noreply.github.com>
2021-07-06 11:10:17 +0100

    split into two r scripts

--- rangesurvey/R/get_respondent_characters.R
+++ rangesurvey/R/get_respondent_characters.R
@@ -1,60 +1,26 @@
 #' get_respondent_characters
 #'
-#' @param data 
+#' @param data whole survey data
 #'
-#' @return
+#' @return respondent character table
 #' @export
 #'
-get_respondent_characters <- function(data){
-  
-  data %>% 
-    dplyr::select("id",
-                    dplyr::contains("GRecorderInfo"),
-                    dplyr::contains("Gawareness"),
-                    dplyr::contains("ertEq"),
-                    "ClimateEq",
-                  dplyr::contains("VoN"),
-                  dplyr::contains("associations"),
-                  dplyr::contains("GDemographics"),
-                  dplyr::contains("Array"),
-                  dplyr::contains("Dual"),
-                 "FinalClimateCheck",
-                 "GFreeFromFinal"
-    ) %>% 
-    dplyr::mutate(id = as.factor(id))
-  
-}
-
-#' get_species_responses
-#'
-#' @param data 
-#'
-#' @return
-#' @export
-#'
-get_species_responses <- function(data) {
-  
- data <- data %>% 
+get_respondent_characters <- function(data) {
+  data %>%
     dplyr::select(
-      -c(dplyr::contains("GRecorderInfo"),
-                  dplyr::contains("Gawareness"),
-                  dplyr::contains("ertEq"),
-                  "ClimateEq",
-                  dplyr::contains("VoN"),
-                  dplyr::contains("associations"),
-                  dplyr::contains("GDemographics"),
-                  dplyr::contains("Array"),
-                  dplyr::contains("Dual"),
-                  "FinalClimateCheck",
-                  "GFreeFromFinal"
-    )
-    )%>% 
-   dplyr::mutate(id = as.factor(id))
- 
-  
-  return(data)
-    
-
-  
+      "id",
+      dplyr::contains("GRecorderInfo"),
+      dplyr::contains("Gawareness"),
+      dplyr::contains("ertEq"),
+      "ClimateEq",
+      dplyr::contains("VoN"),
+      dplyr::contains("associations"),
+      dplyr::contains("GDemographics"),
+      dplyr::contains("Array"),
+      dplyr::contains("Dual"),
+      "FinalClimateCheck",
+      "GFreeFromFinal"
+    ) %>%
+    dplyr::mutate(id = as.factor(.data$id))
 }
 
