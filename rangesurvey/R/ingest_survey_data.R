7e365215bfad7737955acffedb7c3934238d1b9a
JamieCranston <jcranston@protonmail.com>
2022-03-20 09:25:30 +0000

    update ingest function

--- rangesurvey/R/ingest_survey_data.R
+++ rangesurvey/R/ingest_survey_data.R
@@ -8,7 +8,7 @@
 ingest_survey_data <- function(config) {
   survey_data_file_path <- paste0(
     config$data_dir,
-    "FinalSurveyResults.csv"
+    config$data_file
   )
   survey_data <- readr::read_csv(survey_data_file_path)
   return(survey_data)
